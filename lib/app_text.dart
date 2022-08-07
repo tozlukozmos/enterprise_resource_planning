ac0eedf3f7d24d8126cd109f1182492f579584a4
fceviik <ffceviiik38@gmail.com>
2022-08-07 15:08:18 +0300

    text styles added

--- lib/app_text.dart
+++ lib/app_text.dart
@@ -2,4 +2,70 @@ import 'package:flutter/material.dart';
 
 class AppText {
 
+  Widget header({
+    required String data,
+    required Color color,
+  }) {
+    return Text(
+      data,
+      style: TextStyle(
+          fontSize: 20, color: color, fontFamily: "SourceSansProBold"),
+    );
+  }
+
+  Widget label({
+    required String data,
+    required Color color,
+  }) {
+    return Text(
+      data,
+      style: TextStyle(
+          fontSize: 14, color: color, fontFamily: "SourceSansPro"),
+    );
+  }
+
+  Widget labelBold({
+    required String data,
+    required Color color,
+  }) {
+    return Text(
+      data,
+      style: TextStyle(
+          fontSize: 14, color: color, fontFamily: "SourceSansProBold"),
+    );
+  }
+
+  Widget context({
+    required String data,
+    required Color color,
+  }) {
+    return Text(
+      data,
+      style: TextStyle(
+          fontSize: 16, color: color, fontFamily: "SourceSansPro"),
+    );
+  }
+
+  Widget contextBold({
+    required String data,
+    required Color color,
+  }) {
+    return Text(
+      data,
+      style: TextStyle(
+          fontSize: 16, color: color, fontFamily: "SourceSansProBold"),
+    );
+  }
+
+  Widget contextSmall({
+    required String data,
+    required Color color,
+  }) {
+    return Text(
+      data,
+      style: TextStyle(
+          fontSize: 12, color: color, fontFamily: "SourceSansPro"),
+    );
+  }
+
 }
